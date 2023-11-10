d8b4bce18cc252d2e68a2e59940449612cd626d9
Bektas Turzhanov <bektasturzanov@gmail.com>
2023-11-11 00:40:19 +0600

    based on address

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+// src/App.js
+import React from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Bet from './components/Bet.jsx';
 
-function App() {
+const App = () => {
+  const tokenContractAddress = '0x26F40E998788a7C21d08786A2360eef3C433e97f';
+  const tokenContractAbi = /* Your token contract ABI here */;
+
+  const weatherContractAddress = '0x99e9cDd5694fFE85e62B15e7691b3Ab6487E12F0';
+  const weatherContractAbi = /* Your weather contract ABI here */;
+
   return (
     <>
-      <div>
+       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -15,9 +22,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <Bet />
+      <h1>Weather Betting App</h1>
+      <Bet
+        contractAddress={tokenContractAddress}
+        contractAbi={tokenContractAbi}
+        weatherContractAddress={weatherContractAddress}
+        weatherContractAbi={weatherContractAbi}
+      />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Bet.jsx
+++ frontend/src/components/Bet.jsx
@@ -1,122 +1,54 @@
+// src/components/Bet.js
 import React, { useState } from 'react';
-import { useQuery, useMutation, QueryClient, QueryClientProvider } from 'react-query';
 import { ethers } from 'ethers';
 
-const WeatherBettingAddress = 'YOUR_WEATHER_BETTING_CONTRACT_ADDRESS';
-const WeatherBetTokenAddress = 'YOUR_WEATHER_BET_TOKEN_CONTRACT_ADDRESS';
+const Bet = ({ contractAddress, contractAbi, weatherContractAddress, weatherContractAbi }) => {
+  const [betType, setBetType] = useState('');
+  const [betAmount, setBetAmount] = useState('');
+  // Add other state variables as needed
 
-const queryClient = new QueryClient();
+  const connectToContract = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
 
-async function fetchBets() {
-  // Implement fetching bets from the smart contract
-  // Example: const bets = await weatherBettingContract.getAllBets();
-  return [];
-}
+      // Create a contract instance for Weather Betting
+      const weatherContract = new ethers.Contract(weatherContractAddress, weatherContractAbi, signer);
 
-async function fetchWeatherData() {
-  // Implement fetching weather data from the smart contract
-  // Example: const weatherData = await weatherBettingContract.getWeatherData();
-  return { temperature: 25, windSpeed: 5 };
-}
+      // Create a contract instance for Token
+      const contract = new ethers.Contract(contractAddress, contractAbi, signer);
 
-async function placeBet(newBet) {
-  // Implement placing a bet in the smart contract
-  // Example: await weatherBettingContract.placeBet(newBet);
-}
+      // Perform contract operations
+      // Example: const allowance = await contract.allowance(owner, spender);
 
-function Bet() {
-  const [betType, setBetType] = useState('Temperature');
-  const [direction, setDirection] = useState('Higher');
-  const [value, setValue] = useState('');
-  const [tokenAmount, setTokenAmount] = useState('');
+      // You can interact with the contracts using the 'contract' instances
+      // Add your contract interactions here
 
-  const betsQuery = useQuery('bets', fetchBets);
-  const weatherDataQuery = useQuery('weatherData', fetchWeatherData);
-
-  const placeBetMutation = useMutation(placeBet, {
-    onSuccess: () => {
-      // Refetch bets and weather data after a successful bet placement
-      queryClient.invalidateQueries('bets');
-      queryClient.invalidateQueries('weatherData');
-    },
-  });
-
-  const handlePlaceBet = async () => {
-    const newBet = {
-      betType,
-      direction,
-      value: Number(value),
-      tokenAmount: ethers.utils.parseEther(tokenAmount).toString(), // Convert ETH to Wei
-    };
-
-    placeBetMutation.mutate(newBet);
+    } catch (error) {
+      console.error('Error connecting to contract:', error);
+    }
   };
 
   return (
     <div>
-      <h1>Weather Betting App</h1>
-      <div>
-        <label>
-          Bet Type:
-          <select value={betType} onChange={(e) => setBetType(e.target.value)}>
-            <option value="Temperature">Temperature</option>
-            <option value="WindSpeed">Wind Speed</option>
-          </select>
-        </label>
-      </div>
-      <div>
-        <label>
-          Direction:
-          <select value={direction} onChange={(e) => setDirection(e.target.value)}>
-            <option value="Higher">Higher</option>
-            <option value="Lower">Lower</option>
-          </select>
-        </label>
-      </div>
-      <div>
-        <label>
-          Value:
-          <input type="number" value={value} onChange={(e) => setValue(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Token Amount:
-          <input type="number" value={tokenAmount} onChange={(e) => setTokenAmount(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <button onClick={handlePlaceBet} disabled={placeBetMutation.isLoading}>
-          Place Bet
-        </button>
-      </div>
-      <div>
-        <h2>Current Weather Data</h2>
-        {weatherDataQuery.isLoading ? (
-          <p>Loading weather data...</p>
-        ) : (
-          <>
-            <p>Temperature: {weatherDataQuery.data.temperature}°C</p>
-            <p>Wind Speed: {weatherDataQuery.data.windSpeed} m/s</p>
-          </>
-        )}
-      </div>
-      <div>
-        <h2>Current Bets</h2>
-        {betsQuery.isLoading ? (
-          <p>Loading bets...</p>
-        ) : (
-          <ul>
-            {betsQuery.data.map((bet) => (
-              <li key={bet.id}>
-                Bet Type: {bet.betType}, Direction: {bet.direction}, Value: {bet.value}, Amount: {bet.tokenAmount}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+      <h2>Weather Betting App</h2>
+      {/* Add your UI components and form elements */}
+      <label>
+        Bet Type:
+        <select value={betType} onChange={(e) => setBetType(e.target.value)}>
+          <option value="Temperature">Temperature</option>
+          <option value="WindSpeed">Wind Speed</option>
+        </select>
+      </label>
+
+      <label>
+        Bet Amount:
+        <input type="number" value={betAmount} onChange={(e) => setBetAmount(e.target.value)} />
+      </label>
+
+      <button onClick={connectToContract}>Connect to Contract</button>
     </div>
   );
-}
+};
 
 export default Bet;
