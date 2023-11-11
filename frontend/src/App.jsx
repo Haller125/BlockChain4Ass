cacff754e1825e6f8eefa9a49995d06504d8d4bb
Haller125 <erokkabash1@gmail.com>
2023-11-11 07:17:27 +0500

    problems of packages solving

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { useState } from 'react';
 import { ethers } from 'ethers';
 import { tokenAddress as tokenContractAddress, weatherBettingAddress as weatherContractAddress } from "./abi/addreses.js"
 import tokenContractAbi from "./abi/tokenAbi.js"
@@ -10,63 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AccountProfileComponent from './Profile.jsx';
 import { Container } from 'react-bootstrap';
 import React, { useEffect, useState } from 'react';
-import Web3Modal from 'web3modal';
-import { ethers } from 'ethers';
-import { contractAddress, contractAbi } from './abi/addreses';
-import cron from 'node-cron';
 
 
-    const [provider, setProvider] = useState(null);
-    const [signer, setSigner] = useState(null);
-    const [weatherBettingContract, setWeatherBettingContract] = useState(null);
-
-    useEffect(() => {
-        const web3Modal = new Web3Modal({
-            network: "testnet", // change to your preferred network
-            cacheProvider: true
-        });
-
-        async function connectWallet() {
-            try {
-                const instance = await web3Modal.connect();
-                const provider = new ethers.providers.Web3Provider(instance);
-                const signer = provider.getSigner();
-
-                setProvider(provider);
-                setSigner(signer);
-
-                const contract = new ethers.Contract(contractAddress, contractAbi, signer);
-                setWeatherBettingContract(contract);
-            } catch (error) {
-                console.error("Could not connect to wallet:", error);
-            }
-        }
-
-        connectWallet();
-    }, []);
-
-    const updateWeatherData = async () => {
-        console.log("Updating weather data...");
-        const temperature = 25; // Example temperature
-        const windSpeed = 5; // Example wind speed
-        const timestampMinute = Math.floor(Date.now() / 60000);
-
-        if (weatherBettingContract) {
-            try {
-                const tx = await weatherBettingContract.updateWeatherData(timestampMinute, temperature, windSpeed);
-                await tx.wait();
-                console.log(`Weather data updated for timestamp ${timestampMinute}`);
-            } catch (error) {
-                console.error("Error updating weather data:", error);
-            }
-        }
-    };
-
-
-    // Schedule to run every minute
-    cron.schedule('* * * * *', () => {
-        updateWeatherData();
-    });
 
 
 
@@ -102,6 +46,30 @@ const App = () => {
       alert("MetaMask not found. Please install MetaMask.");
     }
   };
+
+    const updateWeatherData = async () => {
+        console.log("Updating weather data...");
+        const temperature = 25; // Example temperature
+        const windSpeed = 5; // Example wind speed
+        const timestampMinute = Math.floor(Date.now() / 60000);
+
+        if (weatherContract) {
+            try {
+                const tx = await weatherContract.updateWeatherData(timestampMinute, temperature, windSpeed);
+                await tx.wait();
+                console.log(`Weather data updated for timestamp ${timestampMinute}`);
+            } catch (error) {
+                console.error("Error updating weather data:", error);
+            }
+        }
+    };
+    useEffect(() => {
+        setTimeout(() => {
+            updateWeatherData();
+        }, 60000);
+    }, []);
+
+
 
   if (!isConnected) {
     return (
