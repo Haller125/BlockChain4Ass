4210f98cab0d56f9352092ac19f7e2236fc2f979
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2023-11-11 08:08:16 +0600

    refactor: lift up 2

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -1,31 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 
-const AccountProfileComponent = ({ tokenContractAddress, tokenABI }) => {
-    const [account, setAccount] = useState('');
+const AccountProfileComponent = ({ account, tokenContract }) => {
     const [tokenBalance, setTokenBalance] = useState('0');
-    const [provider, setProvider] = useState(null);
 
-    const connectWallet = async () => {
-        if (window.ethereum) {
-            try {
-                await window.ethereum.request({ method: 'eth_requestAccounts' });
-                const provider = new ethers.BrowserProvider(window.ethereum);
-                setProvider(provider);
-                const signer = await provider.getSigner();
-                const address = await signer.getAddress();
-                setAccount(address);
-                await loadTokenBalance(address, provider);
-            } catch (error) {
-                console.error("Error connecting to MetaMask", error);
-            }
-        } else {
-            alert("MetaMask not found. Please install MetaMask.");
-        }
-    };
-
-    const loadTokenBalance = async (address, provider) => {
-        const tokenContract = new ethers.Contract(tokenContractAddress, tokenABI, provider);
+    const loadTokenBalance = async (address) => {
         try {
             const balance = await tokenContract.balanceOf(address);
             const decimals = await tokenContract.decimals();
@@ -38,21 +17,15 @@ const AccountProfileComponent = ({ tokenContractAddress, tokenABI }) => {
     };
 
     useEffect(() => {
-        if (account && provider) {
-            loadTokenBalance(account, provider);
-        }
-    }, [account, provider]);
+        loadTokenBalance(account);
+    }, []);
 
     return (
         <div>
-            {!account ? (
-                <button onClick={connectWallet}>Connect to MetaMask</button>
-            ) : (
-                <div>
-                    <p>Address: {account}</p>
-                    <p>Token Balance: {tokenBalance} WBT</p>
-                </div>
-            )}
+            <div>
+                <p>Address: {account}</p>
+                <p>Token Balance: {tokenBalance} WBT</p>
+            </div>
         </div>
     );
 };
