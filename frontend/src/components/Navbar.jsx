538899aa77d8d1e9b9506342986360f8cfe99e1b
Bektas Turzhanov <bektasturzanov@gmail.com>
2023-11-16 23:14:45 +0600

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { ethers } from 'ethers';
 import '../styles/Navbar.css'; // Import your CSS file for styling
 import logo from '../images/logo.png'; // Import the logo image
+import locationIcon from '../images/location.png'
 
 const Navbar = () => {
     const [isConnected, setIsConnected] = useState(false);
@@ -47,9 +48,9 @@ const Navbar = () => {
                 <li className="navbar-item">
                     <Link to="/weather-betting" className="navbar-link">How it Works</Link>
                 </li>
-                <li className="navbar-item city">
+                <li className="navbar-item">
                     <p>The chosen city</p>
-                    <p>Almaty</p>
+                    <p><img src={locationIcon} alt="Location Icon" />Almaty</p>
                 </li>
                 <li className="navbar-item">
                     {!isConnected ? (
