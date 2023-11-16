5a09ef6a652b3c492bbd6fe793478e5466b9e979
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2023-11-16 16:30:36 +0600

    feat: implement schema for frontend

--- frontend/src/abi/addreses.js
+++ frontend/src/abi/addreses.js
@@ -1,5 +1,4 @@
 export const weatherBetTokenAddress = import.meta.env
   .VITE_WEATHER_BET_TOKEN_ADDRESS;
-console.log(weatherBetTokenAddress);
 export const weatherBettingContractAddress = import.meta.env
   .VITE_WEATHER_BETTING_CONTRACT_ADDRESS;
--- frontend/src/service/weatherService.js
+++ frontend/src/service/weatherService.js
@@ -4,8 +4,10 @@ const apiUrl = "https://api.openweathermap.org/data/3.0/onecall";
 const lat = 51.169392;
 const lon = 71.449074;
 const exclude = "current,minutely,hourly,alerts";
-const appid = import.meta.env.VITE_OPENWEATHERMAP_KEY;
+// const appid = import.meta.env.VITE_OPENWEATHERMAP_KEY;
+const appid = "f1706d87612caed6900d28f07e3f451b";
 const units = "metric";
+const BASE_COEF = 1.2;
 
 async function fetchWeatherData() {
   try {
@@ -21,7 +23,46 @@ async function fetchWeatherData() {
 
     if (response.status === 200) {
       const weatherData = response.data;
-      console.log(weatherData.daily[0].temp.day);
+
+      const convertData = (weatherData, nextDayCount) => {
+        const date = weatherData.dt;
+        const moreThanTemp = weatherData.temp.max;
+        const moreThanTempCoef = BASE_COEF ** nextDayCount;
+        const moreThanTempPlus = weatherData.temp.max * 2;
+        const moreThanTempPlusCoef = BASE_COEF ** (nextDayCount + 1);
+        const lessThanTemp = weatherData.temp.min;
+        const lessThanTempCoef = BASE_COEF ** nextDayCount;
+        const lessThanTempPlus = weatherData.temp.min * 2;
+        const lessThanTempPlusCoef = BASE_COEF ** (nextDayCount + 1);
+        const moreThanWindSpeed = weatherData.wind_speed * 1.5;
+        const moreThanWindSpeedCoef = BASE_COEF ** nextDayCount;
+        const lessThanWindSpeed = weatherData.wind_speed / 1.5;
+        const lessThanWindSpeedCoef = BASE_COEF ** nextDayCount;
+        return {
+          date,
+          moreThanTemp,
+          moreThanTempCoef,
+          moreThanTempPlus,
+          moreThanTempPlusCoef,
+          lessThanTemp,
+          lessThanTempCoef,
+          lessThanTempPlus,
+          lessThanTempPlusCoef,
+          moreThanWindSpeed,
+          moreThanWindSpeedCoef,
+          lessThanWindSpeed,
+          lessThanWindSpeedCoef,
+        };
+      };
+
+      let convertedDatas = [];
+
+      weatherData.daily.forEach((weatherData, index) => {
+        const convedtedData = convertData(weatherData, index + 1);
+        convertedDatas.push(convedtedData);
+      });
+
+      console.log(convertedDatas);
     } else {
       console.error("Error fetching weather data:", response.statusText);
     }
