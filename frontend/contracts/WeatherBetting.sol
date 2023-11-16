7d6e8d87840f76b16f59885b8e775375349b5aa2
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2023-11-16 16:38:29 +0600

    feat: change contract so it consider coef and date

--- frontend/contracts/WeatherBetting.sol
+++ frontend/contracts/WeatherBetting.sol
@@ -5,14 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract WeatherBetting is Ownable {
-    enum BetType {
-        Temperature,
-        WindSpeed
-    }
-    enum Direction {
-        Higher,
-        Lower
-    }
+    enum BetType { Temperature, WindSpeed }
+    enum Direction { Higher, Lower }
 
     struct Bet {
         address bettor;
@@ -20,13 +14,14 @@ contract WeatherBetting is Ownable {
         BetType betType;
         Direction direction;
         uint256 value; // Value to compare against, e.g., temperature in Celsius or wind speed in m/s
-        uint256 timestampMinute;
+        uint256 timestampDay;  // Timestamp representing the day (00:00)
+        uint256 coefficient;
         bool processed;
     }
 
     Bet[] public bets;
-    mapping(uint256 => uint256) public actualTemperature; // Maps UNIX timestamp minute to temperature
-    mapping(uint256 => uint256) public actualWindSpeed; // Maps UNIX timestamp minute to wind speed
+    mapping(uint256 => uint256) public actualTemperature;
+    mapping(uint256 => uint256) public actualWindSpeed;
 
     IERC20 public weatherBetToken;
 
@@ -35,57 +30,49 @@ contract WeatherBetting is Ownable {
     }
 
     function placeBet(
-        BetType _betType,
-        Direction _direction,
-        uint256 _value,
-        uint256 _timestampMinute,
-        uint256 _tokenAmount
+        BetType _betType, 
+        Direction _direction, 
+        uint256 _value, 
+        uint256 _timestampMinute, 
+        uint256 _tokenAmount,
+        uint256 _coefficient
     ) external {
         require(_tokenAmount > 0, "Bet amount must be greater than zero.");
 
-        // Check if the contract has allowance to transfer the specified amount of tokens on behalf of msg.sender
-        uint256 allowance = weatherBetToken.allowance(
-            msg.sender,
-            address(this)
-        );
+        uint256 allowance = weatherBetToken.allowance(msg.sender, address(this));
         require(allowance >= _tokenAmount, "Check the token allowance");
 
-        // Try to transfer tokens from the bettor to the contract
-        bool transferSuccess = weatherBetToken.transferFrom(
-            msg.sender,
-            address(this),
-            _tokenAmount
-        );
+        bool transferSuccess = weatherBetToken.transferFrom(msg.sender, address(this), _tokenAmount);
         require(transferSuccess, "Token transfer failed.");
 
-        // Create and store the bet
+        // Truncate to 00:00 of the day and check it's not in the past
+        uint256 timestampDay = _timestampMinute - (_timestampMinute % 86400);
+        require(timestampDay >= block.timestamp - (block.timestamp % 86400), "Cannot place bet for past date");
+
         Bet memory newBet = Bet({
             bettor: msg.sender,
             amount: _tokenAmount,
             betType: _betType,
             direction: _direction,
             value: _value,
-            timestampMinute: _timestampMinute,
+            timestampDay: timestampDay,
+            coefficient: _coefficient,
             processed: false
         });
 
         bets.push(newBet);
     }
 
-    function updateWeatherData(
-        uint256 _timestampMinute,
-        uint256 _temperature,
-        uint256 _windSpeed
-    ) external onlyOwner {
-        actualTemperature[_timestampMinute] = _temperature;
-        actualWindSpeed[_timestampMinute] = _windSpeed;
+    function updateWeatherData(uint256 _timestampDay, uint256 _temperature, uint256 _windSpeed) external onlyOwner {
+        // Truncate to 00:00 of the day
+        uint256 timestampDay = _timestampDay - (_timestampDay % 86400);
 
-        // Process all bets for this timestamp minute
+        actualTemperature[timestampDay] = _temperature;
+        actualWindSpeed[timestampDay] = _windSpeed;
+
+        // Process all bets for the day
         for (uint i = 0; i < bets.length; i++) {
-            if (
-                bets[i].timestampMinute == _timestampMinute &&
-                !bets[i].processed
-            ) {
+            if (bets[i].timestampDay == timestampDay && !bets[i].processed) {
                 processBet(i);
             }
         }
@@ -96,37 +83,22 @@ contract WeatherBetting is Ownable {
         bool won = false;
 
         if (bet.betType == BetType.Temperature) {
-            if (
-                bet.direction == Direction.Higher &&
-                actualTemperature[bet.timestampMinute] > bet.value
-            ) {
+            if (bet.direction == Direction.Higher && actualTemperature[bet.timestampDay] > bet.value) {
                 won = true;
-            } else if (
-                bet.direction == Direction.Lower &&
-                actualTemperature[bet.timestampMinute] < bet.value
-            ) {
+            } else if (bet.direction == Direction.Lower && actualTemperature[bet.timestampDay] < bet.value) {
                 won = true;
             }
         } else if (bet.betType == BetType.WindSpeed) {
-            if (
-                bet.direction == Direction.Higher &&
-                actualWindSpeed[bet.timestampMinute] > bet.value
-            ) {
+            if (bet.direction == Direction.Higher && actualWindSpeed[bet.timestampDay] > bet.value) {
                 won = true;
-            } else if (
-                bet.direction == Direction.Lower &&
-                actualWindSpeed[bet.timestampMinute] < bet.value
-            ) {
+            } else if (bet.direction == Direction.Lower && actualWindSpeed[bet.timestampDay] < bet.value) {
                 won = true;
             }
         }
 
         if (won) {
             // For simplicity, we just return twice the bet amount in tokens in case of a win. A real contract might have more complex payout calculations.
-            require(
-                weatherBetToken.transfer(bet.bettor, bet.amount * 2),
-                "Token transfer failed."
-            );
+            require(weatherBetToken.transfer(bet.bettor, bet.amount * bet.coefficient), "Token transfer failed.");
         }
 
         // Mark bet as processed
