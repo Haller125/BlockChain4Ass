e939785f5f6a1093f08abe17a41a01d589e745c8
Haller125 <erokkabash1@gmail.com>
2023-11-17 11:29:23 +0500

    Modal front ++

--- frontend/src/components/BetsPageTable/BetsPageTableRow/ApproveModal/ApproveModal.jsx
+++ frontend/src/components/BetsPageTable/BetsPageTableRow/ApproveModal/ApproveModal.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Button, Form, FormControl, FormGroup, FormLabel} from "react-bootstrap";
 import "./ApproveModal.css"
 import "../../../../styles/WeatherBetting.css"
+import iconX from "../../../../images/x.svg"
 
 const ApproveModal = ({data, show, time, handleClose}) => {
     if(!show){
@@ -22,13 +23,21 @@ const ApproveModal = ({data, show, time, handleClose}) => {
     return (
         <div className="modal-backdrop" onClick={handleClose}>
             <div className="modal-container" onClick={handleContainerClick}>
-                <h2>Bet Approval</h2>
+                <div className="modal-header">
+                    <h2>Bet Approval</h2>
+                    <div className={"img-x"} onClick={handleClose}>
+                        <img src={iconX} alt="X"/>
+                    </div>
+                </div>
                 <Form className="modal-content" onClick={e => e.stopPropagation()}>
                     <FormGroup className="bet-details">
                         <span className="bet-date">{time.getDate()+"/"+(time.getMonth()+1)}</span>
                         <span className="bet-temperature">{data.type + " " + data.temp}</span>
                         <div className="bet-coefficient">
                             <span>{data.coef}</span>
+                        </div>
+                        <div className={"img-x"} >
+                            <img src={iconX} alt="X"/>
                         </div>
                         <FormControl
                             className={"inputBet"}
