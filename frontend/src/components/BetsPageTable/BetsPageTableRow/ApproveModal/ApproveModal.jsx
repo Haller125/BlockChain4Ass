70378ff7803589d202d7920191fe448087ac838c
Haller125 <erokkabash1@gmail.com>
2023-11-17 10:55:25 +0500

    Modal front ++

--- frontend/src/components/BetsPageTable/BetsPageTableRow/ApproveModal/ApproveModal.jsx
+++ frontend/src/components/BetsPageTable/BetsPageTableRow/ApproveModal/ApproveModal.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Form, FormControl, FormGroup, FormLabel} from "react-bootstrap";
 import "./ApproveModal.css"
 import "../../../../styles/WeatherBetting.css"
 
-const ApproveModal = ({data, show, time}) => {
+const ApproveModal = ({data, show, time, handleClose}) => {
     if(!show){
         return
     }
 
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [gain, setGain] = useState(null)
+
+    const handleGain = (e) => {
+        setGain(e.target.value)
+    }
+
+    const handleContainerClick = (e) => {
+        e.stopPropagation();
+    };
+
     return (
-        <div className="modal-backdrop">
-            <div className="modal-container">
+        <div className="modal-backdrop" onClick={handleClose}>
+            <div className="modal-container" onClick={handleContainerClick}>
                 <h2>Bet Approval</h2>
                 <Form className="modal-content" onClick={e => e.stopPropagation()}>
                     <FormGroup className="bet-details">
@@ -23,10 +34,11 @@ const ApproveModal = ({data, show, time}) => {
                             className={"inputBet"}
                             type="number"
                             placeholder="Number of Tokens"
+                            onChange={handleGain}
                         />
                     </FormGroup>
                     <div className="bet-gain">
-                        possible gain: <span>5.4 WBT</span>
+                        possible gain: <span>{Math.round(gain*data.coef*100)/100} WBT</span>
                     </div>
                     <Button variant="primary" className="mainButton">Close</Button>
                 </Form>
